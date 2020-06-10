7f873f150d94f8e098d23f1382280e7312b432dd
limbuu <iammanshi116@gmail.com>
2020-06-10 15:31:54 +0545

    chat app for flask

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,6 @@
 import logging
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -29,12 +29,27 @@ def sessions():
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
-
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    print('received join request: ',data)
+    send(username + ' has entered the room.', room=room)
+    
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    print('received leave request: ',data)
+    send(username + ' has left the room.', room=room)
+    
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
