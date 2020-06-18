cc3d0bc58e22eaeb19839dc8a655c7f667ba6437
limbuu <iammanshi116@gmail.com>
2020-06-18 22:00:52 +0545

    added a route

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, New 7yw7yf7y7ysysd World!'
+    return 'Hello, New cscsvvcdvs 7yw7yf7y7ysysd World!'
+@app.route('/wello')
+def hello_world():
+    return 'Hello, New cscsvvcdvs 7yw7yf7y7ysysd World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
